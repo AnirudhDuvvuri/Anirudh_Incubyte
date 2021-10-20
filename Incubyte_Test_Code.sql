926f16607136a9ea073c3ecf18ffa42fc8c7a14b
AnirudhDuvvuri <87696901+AnirudhDuvvuri@users.noreply.github.com>
2021-10-20 15:56:11 +0530

    Add files via upload

---
+++ Incubyte_Test_Code.sql
@@ -0,0 +1,7 @@
+show databases;
+use incubyte_test;
+CREATE table Patient_Data (Customer_Name varchar(255) NOT NULL,Customer_Id int(18) NOT NULL,  Open_date date NOT NULL, Last_Consulted_Date date NOT NULL,Vaccination_Id char(5) NOT NULL,Dr_Name varchar(45) DEFAULT NULL,State char(5) DEFAULT NULL, Country char(5) NOT NULL, DOB date DEFAULT NULL, Is_Active char(3) NOT NULL, PRIMARY KEY (`Customer_Id`));
+SHOW VARIABLES LIKE "secure_file_priv";
+SET GLOBAL local_infile=1;
+LOAD DATA INFILE 'C:/ProgramData/MySQL/MySQL Server 5.7/Uploads/Patient_Data.csv' INTO TABLE Patient_Data FIELDS TERMINATED BY ',' ENCLOSED BY '"' LINES TERMINATED BY '\n' IGNORE 1 ROWS;
+Select * from Patient_Data;
